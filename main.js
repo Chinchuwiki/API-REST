f6aef887bd7da8485676bf6a151832ad81df4ed9
Gaston Rocha <gstnedgrdrch93@gmail.com>
2023-04-14 22:46:54 -0300

    Poryecto terminado

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const URL = "https://api.thecatapi.com/v1/images/search";
 const img = document.getElementById("imagenAleatoria");
+const botonSiguiente = document.getElementById("botonSiguiente");
 
+const obtenerImagenAleatoria = () => {
 fetch(URL)
   .then((res) => res.json())
   .then((data) => {
     img.src = data[0].url;
   });
+}
+
+  botonSiguiente.addEventListener("click", obtenerImagenAleatoria);
